ccbbf4bc67d14614fff2b20d5a6c9184aa739cdd
Kisung <51307789+KiSungKim23@users.noreply.github.com>
2025-02-14 06:21:34 +0900

    Update BuffInfoScript.cs
    test

--- Assets/Sctipts/Logic/Core/DataManager/ScriptClass/BuffInfoScript.cs
+++ Assets/Sctipts/Logic/Core/DataManager/ScriptClass/BuffInfoScript.cs
@@ -1,18 +1,1 @@
-﻿//using MessagePack;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
 
-//namespace Logic
-//{
-//    [Serializable]
-//    [MessagePackObject]
-//    public class BuffInfoScript
-//    {
-//        //[Key(0)] public Defines.ItemRarity accessoryRarity;
-//        //[Key(1)] public Defines.ItemEquipType accessoryType;
-//        [Key(2)] public int materialEnhanceCount;
-//        [Key(3)] public int materialAccessoryCount;
-//    }
-//}
